57cd2fe7724a3190606bc5a6a712df9d8f2de86e
shan <shanmugam3725@gmail.com>
2023-05-29 13:41:49 +0530

    new method added

--- src/main/java/jenkins/Sample.java
+++ src/main/java/jenkins/Sample.java
@@ -1,5 +1,10 @@
 public class Sample {
 	public static void main(String[] args) {
 		System.out.println("new file");
+		submit();
 	}
-} jspiders
+	public static void submit()
+	{
+		System.out.println("hiii");
+	}
+} 
